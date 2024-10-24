c405faf71b027ae4c91cc6366bd55b363e5ff2e9
dmr617805 <157419308+dmr617805@users.noreply.github.com>
2024-10-24 17:37:17 -0600

    se agrega preview a mainScreen

--- app/src/main/java/com/example/practicacountriesapi/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/practicacountriesapi/ui/screens/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.example.practicacountriesapi.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,14 +12,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,16 +29,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.example.practicacountriesapi.models.Country
 import com.example.practicacountriesapi.ui.navigation.Screen
 import com.example.practicacountriesapi.ui.viewmodels.CountryUiState
 import com.example.practicacountriesapi.ui.viewmodels.CountryViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun MainScreen(navController: NavController, countryViewModel: CountryViewModel) {
 
@@ -49,9 +51,12 @@ fun MainScreen(navController: NavController, countryViewModel: CountryViewModel)
 
         floatingActionButton = {
             FloatingActionButton(
-                onClick = { navController.navigate(Screen.Detail.route) }
+                onClick = { navController.navigate(Screen.Detail.route) },
+                shape = CircleShape,
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
             ) {
-                Icon(Icons.Default.AddCircle, "Add")
+                Icon(Icons.Default.Add, "Add")
             }
         }
 
@@ -117,4 +122,13 @@ fun CountryItem(country: Country) {
 
         }
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    val navController = rememberNavController()
+    val countryViewModel :CountryViewModel = viewModel()
+    MainScreen(navController, countryViewModel )
 }
